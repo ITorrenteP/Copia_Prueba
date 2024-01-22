df81b80e93e006078f324103f45c83b74f6bd5e2
isabella <isatorrente10@gmail.com>
2024-01-22 15:02:32 -0500

    Isa cambios

---
+++ front/my-app/app/pages/socialmedia/page.tsx
@@ -0,0 +1,12 @@
+const SocialMedia = () => {
+    return (
+        <div>
+            <h2>Nuestras Redes Sociales</h2>
+            <a href="https://www.instagram.com/descubriendojuntxs/" target="_blank" rel="noopener noreferrer">
+                <img src="https://img.freepik.com/vector-premium/icono-instagram-logotipo-insignia-moderna_578229-124.jpg" alt="Instagram" style={{width: "100px", height: "100px"}}/>
+            </a>
+        </div>
+    )
+}
+
+export default SocialMedia
